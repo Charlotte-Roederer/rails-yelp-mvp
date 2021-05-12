60929b6d8b266c614aab11140d6daeb10d491ec9
Charlotte Roederer <lemaildecharlotteroederer@gmail.com>
2021-05-12 12:20:07 +0200

    Add Review model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews
   CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
   validates :name, :address, :category, presence: true
   validates :category, :inclusion=> { :in => CATEGORIES }
