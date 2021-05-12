7477b440df5d594009db4f092833e0d4d4348d9d
Charlotte Roederer <lemaildecharlotteroederer@gmail.com>
2021-05-12 16:46:03 +0200

    restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "destroying restaurant"
+Restaurant.destroy_all
+puts "start seed"
+5.times do
+  restaurants = Restaurant.create( 
+   name: Faker::Restaurant.name,
+   address: Faker::Address.full_address,
+   phone_number: Faker::PhoneNumber.cell_phone_in_e164, 
+   category: Restaurant::CATEGORIES.sample
+)
+end
+puts "seed ok"
